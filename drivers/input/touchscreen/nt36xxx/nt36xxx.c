da5576e418b8b00aaa4d50001d86b3fee9c9bd8c
Jhon Diaz <electimon@gmail.com>
2023-03-23 12:39:17 -0300

    Load firmware earlier
    Changed time from 14000 to 7000 msecs

--- drivers/input/touchscreen/nt36xxx/nt36xxx.c
+++ drivers/input/touchscreen/nt36xxx/nt36xxx.c
@@ -1396,7 +1396,7 @@ static int32_t nvt_ts_late_probe(struct i2c_client *client,
 	}
 	INIT_DELAYED_WORK(&ts->nvt_fwu_work, Boot_Update_Firmware);
 	// please make sure boot update start after display reset(RESX) sequence
-	queue_delayed_work(nvt_fwu_wq, &ts->nvt_fwu_work, msecs_to_jiffies(14000));
+	queue_delayed_work(nvt_fwu_wq, &ts->nvt_fwu_work, msecs_to_jiffies(7000));
 #endif
 
 	NVT_LOG("NVT_TOUCH_ESD_PROTECT is %d\n", NVT_TOUCH_ESD_PROTECT);
