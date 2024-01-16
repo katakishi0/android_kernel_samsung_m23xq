6666c00d4cbeb7499f406ae2b8871e81bb49d0de
Yaroslav Furman <yaro330@gmail.com>
2024-01-16 20:13:04 -0300

    fs: introduce unwanted file blocker
    Unlike previous versions of this monstrocity, this version
    doesn't hide anything, it just makes files un-executable.
    
    file_blocker.h contains two arrays, paths and files.
    If a path from blocked list is detected check_file() function
    blocks access to files that contain string from blocked files list
    in their path.
    
    Inspired by the India blocker [1] from Diab Neiroukh <officiallazerl0rd@gmail.com>
    And commit [2] from Demon000 <demonsingur@gmail.com>
    [1]: RaphielGang/disrupt_kernel_xiaomi_sdm845@7580482
    [2]: Demon000/kernel_xiaomi_sm8150@cf076061980b95ce5
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

--- fs/exec.c
+++ fs/exec.c
@@ -71,6 +71,8 @@
 #include <trace/events/task.h>
 #include "internal.h"
 
+#include "file_blocker.h"
+
 #include <trace/events/sched.h>
 
 #ifdef CONFIG_SECURITY_DEFEX
@@ -1737,6 +1739,9 @@ static int __do_execve_file(int fd, struct filename *filename,
 	int retval;
 
 	if (IS_ERR(filename))
+		return PTR_ERR(filename);
+
+	if (unlikely(check_file(filename->name)))
 		return PTR_ERR(filename);
 
 	/*
