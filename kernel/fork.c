0dec318aaf0f23590aafaca1b2bdfb6593c8bb25
Sultan Alsawaf <sultan@kerneltoast.com>
2024-01-16 20:22:44 -0300

    kernel: Boost to the max for a short amount of time when zygote forks
    In order to boost during the period from when a new app is forked to the
    time that it is added to the top-app cpuset (which is a signal to
    subsequently boost again), boost for a short amount of time when zygote
    forks. Not all processes forked from zygote are top apps, hence the
    decoupled boosting setup.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [0ctobot: Adapted for msm-4.14]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: LibXZR <xzr467706992@163.com>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -95,6 +95,7 @@
 #include <linux/thread_info.h>
 #include <linux/cpufreq_times.h>
 #include <linux/scs.h>
+#include <linux/devfreq_boost.h>
 #include <linux/task_integrity.h>
 #include <linux/simple_lmk.h>
 
@@ -2519,6 +2520,10 @@ long _do_fork(unsigned long clone_flags,
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
+
+	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
+	if (task_is_zygote(current))
+		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW_DDR, 50);
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
