6a5e52f4698ea0ebb0dc31c83aab4fdc32c45d8e
Aflaungos <vimavick@gmail.com>
2024-01-28 04:00:03 -0300

    Revert "ANDROID: sched: Introducing PELT multiplier"
    This reverts commit f33c98cf8444a3b4c5953dcafd0e3de391d49104.

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1003,7 +1003,6 @@ struct rq {
 	u64			clock;
 	/* Ensure that all clocks are in the same cache line */
 	u64			clock_task ____cacheline_aligned;
-	u64			clock_task_mult;
 	u64			clock_pelt;
 	unsigned long		lost_idle_time;
 
@@ -1235,14 +1234,6 @@ static inline u64 rq_clock_task(struct rq *rq)
 	assert_clock_updated(rq);
 
 	return rq->clock_task;
-}
-
-static inline u64 rq_clock_task_mult(struct rq *rq)
-{
-	lockdep_assert_rq_held(rq);
-	assert_clock_updated(rq);
-
-	return rq->clock_task_mult;
 }
 
 static inline void rq_clock_skip_update(struct rq *rq)
