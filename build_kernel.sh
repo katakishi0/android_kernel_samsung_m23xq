8f829a4802ea40d95ec8b258d77978d94d15a215
Aflaungos <vimavick@gmail.com>
2024-02-27 15:30:40 -0300

    Fix some errors

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,14 +2,12 @@
 
 export ARCH=arm64
 export PROJECT_NAME=m23xq
-mkdir out
-
-export BUILD_CROSS_COMPILE=${HOME}/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export KERNEL_LLVM_BIN=${HOME}/llvm-arm-toolchain-ship/bin/clang
+export CROSS_COMPILE=${HOME}/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CC=${HOME}/linux-x86/clang-r416183b/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
-export KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+export DTC_EXT=$(pwd)/tools/dtc
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CONFIG_SECTION_MISMATCH_WARN_ONLY=y vendor/m23xq_eur_open_defconfig
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+make -j$(nproc --all) -C $(pwd) O=$(pwd)/out CC=clang vendor/m23xq_eur_open_defconfig
+make -j$(nproc --all) -C $(pwd) O=$(pwd)/out CC=clang
 
 $(pwd)/tools/mkdtimg create $(pwd)/out/arch/arm64/boot/dtbo.img --page_size=4096 $(find out/arch/arm64/boot/dts/samsung/m23/m23xq/ -name *.dtbo)
