313b33a018cdee64b0f9b577e2ec4968ea601e5d
Aflaungos <vimavick@gmail.com>
2024-03-11 02:38:58 -0300

    Revert "techpack: display: msm: dsi: add get_main_display"
    This reverts commit 71c313896d3eae5dca26d317e6b1bc0890c0fb00.

--- techpack/display/msm/dsi/dsi_display.c
+++ techpack/display/msm/dsi/dsi_display.c
@@ -58,8 +58,6 @@ static const struct of_device_id dsi_display_dt_match[] = {
 	{}
 };
 
-struct dsi_display *primary_display;
-
 static void dsi_display_mask_ctrl_error_interrupts(struct dsi_display *display,
 			u32 mask, bool enable)
 {
@@ -6766,7 +6764,6 @@ int dsi_display_get_modes(struct dsi_display *display,
 exit:
 	*out_modes = display->modes;
 	rc = 0;
-	primary_display = display;
 
 error:
 	if (rc)
@@ -8511,10 +8508,6 @@ int dsi_display_unprepare(struct dsi_display *display)
 #endif
 
 	return rc;
-}
-
-struct dsi_display *get_main_display(void) {
-	return primary_display;
 }
 
 static int __init dsi_display_register(void)
